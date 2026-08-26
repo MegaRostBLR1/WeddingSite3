6b25fa6216203ed23322d9aa1cca35b777167b32
Ростислав Якубовский <110860900+MegaRostBLR1@users.noreply.github.com>
2026-08-26 23:10:04 +0300

    refactor: add wedding configuration

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.WEDDING_CONFIG = Object.freeze({
+  couple: {
+    bride: 'Анна',
+    groom: 'Михаил',
+  },
+  weddingDate: '2026-09-26T15:00:00+03:00',
+  displayDate: '26 сентября 2026',
+  location: 'Усадьба «Оливковая роща»',
+});
