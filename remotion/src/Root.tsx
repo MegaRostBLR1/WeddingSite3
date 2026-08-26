ab659bd9ed425af47959064e5b1e8a64bfa769f7
Ростислав Якубовский <110860900+MegaRostBLR1@users.noreply.github.com>
2026-08-26 23:10:18 +0300

    feat: register WeddingSalesReel composition

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Composition } from 'remotion';
 import { WeddingReels } from './WeddingReels';
+import { WeddingSalesReel } from './WeddingSalesReel';
 
 export const RemotionRoot: React.FC = () => (
-  <Composition
-    id="WeddingReels"
-    component={WeddingReels}
-    durationInFrames={600}
-    fps={30}
-    width={1080}
-    height={1920}
-    defaultProps={{}}
-  />
+  <>
+    <Composition
+      id="WeddingReels"
+      component={WeddingReels}
+      durationInFrames={600}
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{}}
+    />
+    <Composition
+      id="WeddingSalesReel"
+      component={WeddingSalesReel}
+      durationInFrames={540}
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{}}
+    />
+  </>
 );
